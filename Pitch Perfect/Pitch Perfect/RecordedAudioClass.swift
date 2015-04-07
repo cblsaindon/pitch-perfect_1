060fa45d342dcda3446cb20ad2db22c4a2c93ab3
Candace Saindon <cblsaindon@gmail.com>
2015-04-06 19:19:22 -0600

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudioClass.swift
@@ -0,0 +1,9 @@
+//
+//  RecordedAudioClass.swift
+//  Pitch Perfect
+//
+//  Created by Candace Saindon on 4/6/15.
+//  Copyright (c) 2015 Candace Saindon. All rights reserved.
+//
+
+import Foundation
